171404b233456ec36c415565110265e4dda6e383
Youssef-S-Negm <youssef.negm2001@gmail.com>
2025-01-07 17:39:41 +0200

    First commit

---
+++ src/main/java/com/youssef/doablebackend/DoablebackendApplication.java
@@ -0,0 +1,13 @@
+package com.youssef.doablebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoablebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoablebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youssef/doablebackend/DoablebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youssef.doablebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoablebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
