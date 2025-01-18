9b811abdeea2638b7748db663c6f1593da8325ee
Youssef-S-Negm <youssef.negm2001@gmail.com>
2025-01-18 22:22:36 +0200

    update: validate request before get, delete, and update. Get user metadata before adding task

--- src/main/java/com/youssef/doablebackend/service/TaskService.java
+++ src/main/java/com/youssef/doablebackend/service/TaskService.java
@@ -4,9 +4,11 @@ import com.youssef.doablebackend.dao.TaskRepository;
 import com.youssef.doablebackend.dao.UserRepository;
 import com.youssef.doablebackend.entity.Task;
 import com.youssef.doablebackend.entity.User;
+import com.youssef.doablebackend.exception.NotUserTaskException;
 import com.youssef.doablebackend.exception.TaskNotFoundException;
-import com.youssef.doablebackend.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,48 +29,63 @@ public class TaskService implements ITaskService {
     @Override
     @Transactional
     public void save(Task task) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
+
+        task.setUserId(user.getId());
         taskRepository.save(task);
     }
 
     @Override
     public Task findById(String id) throws TaskNotFoundException {
         Optional<Task> result = taskRepository.findById(id);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
 
         if (result.isEmpty()) throw new TaskNotFoundException("Task id - " + id + " not found");
 
-        return result.get();
+        Task task = result.get();
+
+        if (!task.getUserId().equals(user.getId()))
+            throw new NotUserTaskException("task id - " + id + " is not owned by user id - " + user.getId());
+
+        return task;
     }
 
     @Override
     @Transactional
-    public void deleteById(String id) throws TaskNotFoundException {
+    public void deleteById(String id) {
         Optional<Task> result = taskRepository.findById(id);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
 
-        if (result.isEmpty()) throw new TaskNotFoundException("Task id - " + id + " not found");
+        if (result.isEmpty())
+            throw new TaskNotFoundException("Task id - " + id + " not found");
 
-        taskRepository.delete(result.get());
+        Task task = result.get();
+
+        if (!task.getUserId().equals(user.getId()))
+            throw new NotUserTaskException("task id - " + id + " is not owned by user id - " + user.getId());
+
+        taskRepository.delete(task);
     }
 
     @Override
     @Transactional
     public void updateStatusById(String id, String status) throws TaskNotFoundException {
         Optional<Task> result = taskRepository.findById(id);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
 
-        if (result.isEmpty()) throw new TaskNotFoundException("Task id - " + id + " not found");
+        if (result.isEmpty())
+            throw new TaskNotFoundException("Task id - " + id + " not found");
 
-        result.get().setStatus(status);
-        taskRepository.save(result.get());
-    }
+        Task task = result.get();
 
-    @Override
-    @Transactional
-    public void addTaskToUser(String id, Task task) throws UserNotFoundException {
-        Optional<User> result = userRepository.findById(id);
+        if (!task.getUserId().equals(user.getId()))
+            throw new NotUserTaskException("task id - " + id + " is not owned by user id - " + user.getId());
 
-        if (result.isEmpty()) throw new UserNotFoundException("User id - " + id + " not found");
-
-        task.setUserId(result.get().getId());
-
+        task.setStatus(status);
         taskRepository.save(task);
     }
 }
