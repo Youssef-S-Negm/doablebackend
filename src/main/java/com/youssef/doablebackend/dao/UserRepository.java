1452357b5bd270303769e3f6f8c2968674c7eaa6
Youssef-S-Negm <youssef.negm2001@gmail.com>
2025-01-18 22:15:00 +0200

    update: add findByEmail method

--- src/main/java/com/youssef/doablebackend/dao/UserRepository.java
+++ src/main/java/com/youssef/doablebackend/dao/UserRepository.java
@@ -2,6 +2,10 @@ package com.youssef.doablebackend.dao;
 
 import com.youssef.doablebackend.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface UserRepository extends MongoRepository<User, String> {
+
+    @Query("{'email': ?0}")
+    User findByEmail(String email);
 }
